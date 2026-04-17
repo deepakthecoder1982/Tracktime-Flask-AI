f91b6aa389cf43e76cb4dd83fbd436c35b1b476d
Deepak Yadav <108220666+deepakthecoder1982@users.noreply.github.com>
2026-04-17 09:20:58 +0000

    done

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ CORS(app)
 # Database configuration
 try:
     logging.info("Setting up database connection...")
-    engine = create_engine(f'mysql+mysqlconnector://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}')
+    engine = create_engine(f'postgresql+psycopg2://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}')
     Session = sessionmaker(bind=engine)
     logging.info("Database connection established.")
 except Exception as e:
